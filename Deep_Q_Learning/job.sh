f525384eef2cdc5ad6070c1c9bd426db06da62f0
hitesh-vs <vshitesh17@gmail.com>
2024-10-24 20:22:40 -0400

    Added job.sh

---
+++ Deep_Q_Learning/job.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+SBATCH --mail-user=sviswasam@wpi.edu
+SBATCH --mail-type=ALL
+
+#SBATCH -A cs525
+#SBATCH -p academic
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --gres=gpu:1
+#SBATCH -C A30
+#SBATCH -t 24:00:00
+#SBATCH --mem 12G
+#SBATCH --job-name="P3"
+
+SBATCH --output=/home/sviswasam/rl/logs/logs.out
+SBATCH --error=/home/sviswasam/rl/logs/err.err
+
+source activate myenv
+python main.py --train_dqn
